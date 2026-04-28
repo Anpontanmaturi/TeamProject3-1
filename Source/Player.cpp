c76e3825aea4d280fb5702e96f1078f3c2d35433
Anpontanmaturi <2240070@ecc.ac.jp>
2026-04-28 16:03:38 +0900

    player model

--- Source/Player.cpp
+++ Source/Player.cpp
@@ -8,22 +8,13 @@
 #include "ProjectileHoming.h"
 #include <SceneGame.h>
 
-/*//コンストラクタ
-Player::Player()
-{
-	model = new Model("Data/Model/Mr.Incredible/Mr.Incredible.mdl");
-
-	//モデルが大きいのでスケーリング
-	scale.x = scale.y = scale.z = 0.01f;
-}*/
-
 //初期化
 void Player::Initialize()
 {
-	model = new Model("Data/Model/Mr.Incredible/Mr.Incredible.mdl");
+	model = new Model("Data/Model/Player/runba_on_tex.mdl");
 
 	//モデルが大きいのでスケーリング
-	scale.x = scale.y = scale.z = 0.01f;
+	scale.x = scale.y = scale.z = 0.025f;
 }
 
 void Player::AddEnergy(float value)
@@ -37,12 +28,6 @@ void Player::AddEnergy(float value)
 	}
 }
 
-/*//デストラクタ
-Player::~Player()
-{
-	delete model;
-}*/
-
 //終了化
 void Player::Finalize()
 {
@@ -52,47 +37,8 @@ void Player::Finalize()
 //更新処理
 void Player::Update(float elapsedTime)
 {
-	/*//信仰ベクトル取得
-	DirectX::XMFLOAT3 moveVec = GetMoveVec();
-
-	//移動処理
-	float moveSpeed = this->moveSpeed * elapsedTime;
-	position.x += moveVec.x * moveSpeed;
-	position.y += moveVec.y * moveSpeed;*/
-
 	//移動入力処理
 	InputMove(elapsedTime);
-
-	/*//入力情報を取得
-	GamePad& gamePad = Input::Instance().GetGamePad();
-	float ax = gamePad.GetAxisLX();
-	float ay = gamePad.GetAxisLY();
-
-	//移動操作
-	float moveSpeed = 5.0f * elapsedTime;
-	{
-		//左スティックの入力情報をもとにXZ平面への移動処理
-		position.x += ax * moveSpeed;
-		position.z += ay * moveSpeed;
-	}
-
-	//回転操作
-	float rotateSpeed = DirectX::XMConvertToRadians(360) * elapsedTime;
-	if (gamePad.GetButton() & GamePad::BTN_A)
-	{
-		//X軸回転操作
-		angle.x += rotateSpeed;
-	}
-	if (gamePad.GetButton() & GamePad::BTN_B)
-	{
-		//Y軸回転操作
-		angle.y += rotateSpeed;
-	}
-	if (gamePad.GetButton() & GamePad::BTN_X)
-	{
-		//Z軸回転操作
-		angle.z += rotateSpeed;
-	}*/
 
 	//ジャンプ入力処理
 	InputJump();
@@ -119,58 +65,6 @@ void Player::Update(float elapsedTime)
 	model->UpdateTransform();
 	
 }
-
-/*//移動処理
-void Player::Move(float elapsedTime, float vx, float vz, float speed)
-{
-	speed *= elapsedTime;
-	position.x += vx * speed;
-	position.z += vz * speed;
-}
-
-旋回処理
-void Player::Turn(float elapsedTime, float vx, float vz, float speed)
-{
-	speed *= elapsedTime;
-
-	//進行ベクトルがゼロベクトルの場合は処理する必要なし
-	float length = sqrtf(vx * vx + vz * vz);
-	if (length < 0.001f)return;
-
-	//進行ベクトルを単位ベクトル化
-	vx /= length;
-	vz /= length;
-
-	//自身の回転値から前方向を求める
-	float frontX = sinf(angle.y);
-	float frontZ = cosf(angle.y);
-	
-	//回転角を求めるため。2つの単位ベクトルの内積を計算する
-	float dot = (frontX * vx) + (frontZ * vz);
-
-	//内積値は-1.0～1.0で表現されており、２つの単位ベクトルの角度が
-	//小さいほど1.0に近づくという性質を利用して回転角度を調整する
-	float rot = 1.0f - dot;
-	if (rot > speed) rot = speed;
-
-	//左右判定を行うために２つの単位ベクトルの外積を計算する
-	float cross = (frontZ * vx) - (frontX * vz);
-
-	//2Dの外積が正の場合か負の場合かによって左右判定が行える
-	//左右判定を行うことによって左右回転を選択する
-	if (cross < 0.0f)
-	{
-		//angle.y -= speed;
-		angle.y -= rot;
-	}
-	else
-	{
-		//angle.y += speed;
-		angle.y += rot;
-	}
-
-	
-}*/
 
 //着地した時に呼ばれる
 void Player::OnLanding()
@@ -330,15 +224,6 @@ void Player::CollisionPlayerVsEnemies()
 
 		//衝突処理
 		DirectX::XMFLOAT3 outPosition;
-		//if (Collision::IntersectSphereVsSphere(
-		//	position, radius,
-		//	enemy->GetPosition(),
-		//	enemy->GetRadius(),
-		//	outPosition))
-		//{
-		//	//押し出し後の位置判定
-		//	enemy->SetPosition(outPosition);
-		//}
 		if (Collision::IntersectCylinderVsCylinder(
 			position,
 			radius,
@@ -349,9 +234,6 @@ void Player::CollisionPlayerVsEnemies()
 			outPosition
 		))
 		{
-			////押し出し後の位置設定
-			//enemy->SetPosition(outPosition);
-
 			//敵の真上に当たったかを判定
 			DirectX::XMVECTOR P = DirectX::XMLoadFloat3(&position);
 			DirectX::XMVECTOR E = DirectX::XMLoadFloat3(&enemy->GetPosition());
@@ -412,31 +294,6 @@ void Player::CollisionPlayerVsEnemies()
 	}
 }
 
-//ジャンプ処理
-//void Player::Jump(float speed)
-//{
-//	//上方向の力を設定
-//	velocity.y = speed;
-//}
-
-//速力処理更新
-//void Player::UpdateVelocity(float elapsedTime)
-//{
-//	//重力処理
-//	velocity.y += gravity * elapsedTime;
-//
-//	//移動処理
-//	position.y += velocity.y * elapsedTime;
-//
-//	//地面判定
-//	if (position.y < 0.0f)
-//	{
-//		position.y = 0.0f;
-//		velocity.y = 0.0f;
-//	}
-//}
-
-
 void Player::Heal(float amount)
 {
 	hp += amount;
@@ -471,74 +328,6 @@ void Player::InputProjectile()
 		//追加
 		energy = maxenergy;
 	}
-
-	////直進弾丸発射
-	//if (gamePad.GetButtonDown() & GamePad::BTN_X)
-	//{
-	//	//前方向
-	//	DirectX::XMFLOAT3 dir;
-	//	dir.x = sinf(angle.y);
-	//	dir.y = 0.0f;
-	//	dir.z = cosf(angle.y);
-	//	//発射位置（プレイヤーの腰あたり）
-	//	DirectX::XMFLOAT3 pos;
-	//	pos.x = position.x;
-	//	pos.y = position.y + height * 0.5f;
-	//	pos.z = position.z;
-	//	//発射
-	//	ProjectileStraight* projectile = new ProjectileStraight(&projectileManager);
-	//	projectile->Launch(dir, pos);
-	//	//projectileManager.Register(projectile);
-	//}
-	////追尾弾丸発射
-	//if (gamePad.GetButtonDown() & GamePad::BTN_Y)
-	//{
-	//	//追加
-	//	energy = maxenergy;
-	//	//前方向
-	//	DirectX::XMFLOAT3 dir;
-	//	dir.x = sinf(angle.y);
-	//	dir.y = 0.0f;
-	//	dir.z = cosf(angle.y);
-	//
-	//	//発射位置（プレイヤーの腰あたり）
-	//	DirectX::XMFLOAT3 pos;
-	//	pos.x = position.x;
-	//	pos.y = position.y + height * 0.5f;
-	//	pos.z = position.z;
-	//
-	//	//ターゲット（デフォルトではプレイヤーの前方）
-	//	DirectX::XMFLOAT3 target;
-	//	target.x = pos.x + dir.x * 1000.0f;
-	//	target.y = pos.y + dir.y * 1000.0f;
-	//	target.z = pos.z + dir.z * 1000.0f;
-	//
-	//	//一番近くの敵をターゲットにする
-	//	float dist = FLT_MAX;
-	//	EnemyManager& enemyManager = EnemyManager::Instance();
-	//	int enemyCount = enemyManager.GetEnemyCount();
-	//	for (int i = 0; i < enemyCount; ++i)
-	//	{
-	//		//敵との距離判定
-	//		Enemy* enemy = EnemyManager::Instance().GetEnemy(i);
-	//		DirectX::XMVECTOR P = DirectX::XMLoadFloat3(&position);
-	//		DirectX::XMVECTOR E = DirectX::XMLoadFloat3(&enemy->GetPosition());
-	//		DirectX::XMVECTOR V = DirectX::XMVectorSubtract(E, P);
-	//		DirectX::XMVECTOR D = DirectX::XMVector3LengthSq(V);
-	//		float d;
-	//		DirectX::XMStoreFloat(&d, D);
-	//		if (d < dist)
-	//		{
-	//			dist = d;
-	//			target = enemy->GetPosition();
-	//			target.y += enemy->GetHeight() * 0.5f;
-	//		}
-	//	}
-	//
-	//	//発射
-	//	ProjectileHoming* projectile = new ProjectileHoming(&projectileManager);
-	//	projectile->Launch(dir, pos, target);
-	//}
 }
 
 //弾丸と敵の衝突処理
