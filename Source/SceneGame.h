40bf7adac6e7d7f68d038fd12f2885faf6f16bc3
Saku46 <2240047@ecc.ac.jp>
2026-05-21 12:25:38 +0900

    リザルト
    リザルト画面の時すぐにタイトルになる時があるもで直した

--- Source/SceneGame.h
+++ Source/SceneGame.h
@@ -94,8 +94,8 @@ private:
 	AudioSource* SGSe2 = nullptr;
 
 #if 1
-	float timeLimit = 180.0f;   // 制限時間（秒）
-	float currentTime = 180.0f; // 残り時間
+	float timeLimit = 5.0f;   // 制限時間（秒）
+	float currentTime = 5.0f; // 残り時間
 #else
 	float timeLimit = 5.0f;   // 制限時間（秒）
 	float currentTime = 5.0f; // 残り時間
